ee035c13d08f5f533947b1c0ef20ea90284ef16c
caiwenming <caiwenming@myhexin.com>
2019-04-10 18:19:40 +0800

    add

--- getErtong/settings.py
+++ getErtong/settings.py
@@ -22,7 +22,7 @@ NEWSPIDER_MODULE = 'getErtong.spiders'
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 1
+# CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay

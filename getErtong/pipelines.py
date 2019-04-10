f086cf611dc85edf59ed8d4433ff1e1a8d2eb060
caiwenming <caiwenming@myhexin.com>
2019-04-10 18:23:59 +0800

    add sql

--- getErtong/pipelines.py
+++ getErtong/pipelines.py
@@ -9,16 +9,15 @@ import pymysql
 
 class GetertongPipeline(object):
     def process_item(self, item, spider):
-        # db = pymysql.connect('localhost', 'root', '897011805', 'yhj')
-        # cursor = db.cursor()
+        db = pymysql.connect('localhost', 'root', '897011805', 'yhj')
+        cursor = db.cursor()
         title = pymysql.escape_string(item['title'])
         detailUrl = pymysql.escape_string(item['detailUrl'])
         content = pymysql.escape_string(item['content'])
         imageUrl = pymysql.escape_string(item['imageUrl'])
         detailcontent = pymysql.escape_string(item['detailcontent'])
         sql = '''INSERT INTO ertong VALUES ('%s', '%s', '%s', '%s, '%s)'''%(title, detailUrl, content, imageUrl, detailcontent)
-        # cursor.execute(sql)
-        # db.commit()
-        # db.close()
-        print(',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,', sql)
+        cursor.execute(sql)
+        db.commit()
+        db.close()
         return item

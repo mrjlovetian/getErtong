5cc9c375224c5f685feef2e5ff25be76625e812d
caiwenming <caiwenming@myhexin.com>
2019-04-10 18:41:45 +0800

    add sql

--- getErtong/pipelines.py
+++ getErtong/pipelines.py
@@ -15,7 +15,7 @@ class GetertongPipeline(object):
         detailUrl = pymysql.escape_string(item['detailUrl'])
         content = pymysql.escape_string(item['content'])
         imageUrl = pymysql.escape_string(item['imageUrl'])
-        detailcontent = pymysql.escape_string(item['detailcontent'])
+        detailcontent = (item['detailcontent'])
         sql = '''INSERT INTO ertong VALUES ('%s', '%s', '%s', '%s, '%s)'''%(title, detailUrl, content, imageUrl, detailcontent)
         cursor.execute(sql)
         db.commit()

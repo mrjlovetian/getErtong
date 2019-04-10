9817f35c67365401bf93419885abe37fb0833a8d
caiwenming <caiwenming@myhexin.com>
2019-04-10 18:22:22 +0800

    add PIPLINE

--- getErtong/settings.py
+++ getErtong/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'getErtong.spiders'
 #USER_AGENT = 'getErtong (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 1
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'getErtong.pipelines.GetertongPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'getErtong.pipelines.GetertongPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
